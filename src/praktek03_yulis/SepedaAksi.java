951710c1612da101044686a9dad7a139994133fc
yulistina <yulistina101@gmail.com>
2022-12-14 19:51:02 +1400

    Buat Class Aksi

---
+++ src/praktek03_yulis/SepedaAksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek03_yulis;
+
+/**
+ *
+ * @author HP 14-an017AU
+ */
+public class SepedaAksi {
+    public static void main(String[] args) {
+        
+    }
+}
