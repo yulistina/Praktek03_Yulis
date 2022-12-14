d472e2b5ba58323b72fd1d2396f7a8848fbf0563
yulistina <yulistina101@gmail.com>
2022-12-14 20:17:14 +1400

    Menggunakan Method get

--- src/praktek03_yulis/SepedaAksi.java
+++ src/praktek03_yulis/SepedaAksi.java
@@ -17,5 +17,12 @@ public class SepedaAksi {
         s.setHarga(1000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t:");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t:");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t:");
+        System.out.println(s.getHarga());
     }
 }
