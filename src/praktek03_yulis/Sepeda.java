ad84b372d9917f1057910df08cfcb2c83520f799
yulistina <yulistina101@gmail.com>
2022-12-14 19:48:06 +1400

    Buat method cetaInfo()

--- src/praktek03_yulis/Sepeda.java
+++ src/praktek03_yulis/Sepeda.java
@@ -5,4 +5,10 @@ public class Sepeda {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("merk \t: "+merk+"\n"+
+                "warna \t: "+warna+"\n"+
+                "harga \t: "+harga+"\n");    
+    }
 }
