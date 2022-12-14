f22d00ddbe795d59603c4de2fd2f9e259c87f299
yulistina <yulistina101@gmail.com>
2022-12-14 19:45:50 +1400

    Membuat Atribut

--- src/praktek03_yulis/Sepeda.java
+++ src/praktek03_yulis/Sepeda.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek03_yulis;
 
-/**
- *
- * @author HP 14-an017AU
- */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
 }
