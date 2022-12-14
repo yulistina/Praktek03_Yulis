fabc9b54eec475c85f42701566b9f36d09059bea
yulistina <yulistina101@gmail.com>
2022-12-14 20:28:16 +1400

    Atribut dan method

--- src/praktek03_yulis/SepedaMotor.java
+++ src/praktek03_yulis/SepedaMotor.java
@@ -1,6 +1,9 @@
 package praktek03_yulis;
 
-
 public class SepedaMotor extends Sepeda{
+    private String jenisMotor;
     
+    void throttle(){
+        System.out.println("NGEEENG......");
+    }
 }
