309ae7e3f1ea5b0b90cc897e8bd35c620966cd80
yulistina <yulistina101@gmail.com>
2022-12-14 20:08:48 +1400

    Menggunakan Method set

--- src/praktek03_yulis/SepedaAksi.java
+++ src/praktek03_yulis/SepedaAksi.java
@@ -12,9 +12,9 @@ package praktek03_yulis;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk="Kupu-Kupu";
-        s.warna="Merak";
-        s.harga=1000;
+        s.setMerk("Butterfly");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }
