bf9cdba5801adeb8896c7996613e77965b868d38
yulistina <yulistina101@gmail.com>
2022-12-14 19:43:45 +1400

    Initial commit

---
+++ src/praktek03_yulis/Praktek03_Yulis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek03_yulis;
+
+/**
+ *
+ * @author HP 14-an017AU
+ */
+public class Praktek03_Yulis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/praktek03_yulis/Sepeda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek03_yulis;
+
+/**
+ *
+ * @author HP 14-an017AU
+ */
+public class Sepeda {
+    
+}
