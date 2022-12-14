1e8cbb0b7df9bc9971967ceac13f103c262c5395
yulistina <yulistina101@gmail.com>
2022-12-14 19:59:40 +1400

    Set atribut private

--- src/praktek03_yulis/Sepeda.java
+++ src/praktek03_yulis/Sepeda.java
@@ -2,9 +2,9 @@
 package praktek03_yulis;
 
 public class Sepeda {
-    String merk;
-    String warna;
-    double harga;
+    private String merk;
+    private String warna;
+    private double harga;
     
     void cetakInfo(){
         System.out.println("merk \t: "+merk+"\n"+
